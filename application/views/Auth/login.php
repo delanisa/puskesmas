3ea0a1dbed97fae0a197b66ce2850a63bde0ad01
delanisa <delaninisat17@gmail.com>
2020-09-11 10:25:52 +0700

    First commit

---
+++ application/views/Auth/login.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title></title>
+	</head>
+	<body class="hold-transition login-page">
+	<div class="login-box">
+	<div class="login-logo">
+		<a href="<?php echo base_url(); ?>"><b>Sistem informasi Puskesmas</b></a>
+	</div>
+	
+	</div>
+	</body>
+</html>
